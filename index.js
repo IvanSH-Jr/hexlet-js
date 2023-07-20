9d2b44d68048dad9d3c987c028021b75c1ab99b8
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-07-20 12:53:34 +0300

    feat: some fn for test prettier

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-import _ from 'lodash';
-console.log(_.last(['one', 'two']));
+import _ from "lodash";
+console.log(_.last(["one", "two"]));
+//тест претира
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
